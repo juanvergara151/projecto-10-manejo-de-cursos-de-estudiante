3cd739c90399328abfb4cf260db1e3d8c12357ca
juanvergara151 <jvergara912@uan.edu.co>
2025-04-26 14:42:15 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+struct estudiante{
+    
+    
+	//atributos
+	int codigoE,eleccion_materias;
+	string nombre, apellido;
+	
+	//metodo
+	int calcular_promedio;
+    
+};
+
+struct materias {
+    
+    //atributos
+    int codigo,nota;
+    string departamento;
+    
+    //metodo
+    int nota_curso,valor_creditos,cantidad_cortes;
+    
+};
+    
+
+int main()
+{
+    
+}
